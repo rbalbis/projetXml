74c48b40bf55f9be6cca2207c5d29fa76942be6d
Balbis Robin <robin.balbis@etu.unice.fr>
2017-12-06 12:52:20 +0100

    changement de package

---
+++ xmlRest/src/main/java/project/TestProjet1.java
@@ -0,0 +1,16 @@
+package project;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("welcome")
+public class TestProjet1 {
+
+	@GET
+	@Produces("text/plain")
+	public String getXml() {
+		return "boujour";
+	}
+
+}
