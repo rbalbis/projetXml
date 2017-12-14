f1205906f91b7c5dc4d21ee0df364c703f5f6201
Balbis Robin <robin.balbis@etu.unice.fr>
2017-12-14 22:06:28 +0100

    xsd class dans le package

--- xmlRest/src/main/java/project/dataflow/MainDataflow.java
+++ xmlRest/src/main/java/project/dataflow/MainDataflow.java
@@ -6,22 +6,24 @@ import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
-import fr.inria.ns.dsi.si.app.bastri.StructureInrias;
-import fr.inria.ns.dsi.si.app.bastri.Structureinria;
+import org.inria.fr.ns.sr.StructureInrias;
+import org.inria.fr.ns.sr.Structureinria;
+
 
 public class MainDataflow {
 
 	public static void main(String[] args) {
 		try {
-		      JAXBContext jc = JAXBContext.newInstance("fr.inria.ns.dsi.si.app.bastri");
+		      JAXBContext jc = JAXBContext.newInstance("org.inria.fr.ns.sr");
 		      Unmarshaller unmarshaller = jc.createUnmarshaller();
 
-		      StructureInrias struc = (StructureInrias) unmarshaller.unmarshal(new File("./src/main/resources/bastri.xml"));
+		      StructureInrias struc = (StructureInrias) unmarshaller.unmarshal(new File("./src/main/resources/sr/bastri.xml"));
 
 		      List<Structureinria> listStruc = struc.getStructureinria();
 		      
 		      for (Structureinria structureinria : listStruc) {
 				System.out.println(structureinria.getLibellefr());
+				System.out.println();
 			}
 		      
 		    
