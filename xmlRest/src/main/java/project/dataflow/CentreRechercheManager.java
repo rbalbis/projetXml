f2bb81ed44d0cf5e4307224e1d73f494c0f5a486
Balbis Robin <robin.balbis@etu.unice.fr>
2017-12-21 12:42:32 +0100

    ajout fonction rest

---
+++ xmlRest/src/main/java/project/dataflow/CentreRechercheManager.java
@@ -0,0 +1,112 @@
+package project.dataflow;
+
+import java.io.File;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.inria.fr.ns.*;
+import org.inria.fr.ns.cr.Crs;
+import org.inria.fr.ns.cr.Crs.Cr;
+import org.inria.fr.ns.cr.Crs.Cr.Adressegeographique;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import project.http.response.CustomResponse;
+import utils.MakeXml;
+
+@Path("cr")
+public class CentreRechercheManager {
+
+	/**
+	 * Liste les centres de recheches
+	 * Service Rest
+	 * @return
+	 */
+	@Path("listcr")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String listCentreRecherche() {
+		JAXBContext jc;
+		final GsonBuilder builder = new GsonBuilder();
+		final Gson gson = builder.create();
+		try {
+			jc = JAXBContext.newInstance("org.inria.fr.ns.cr");
+			String res = "";
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+
+			Crs centreRecherche = (Crs) unmarshaller.unmarshal(new File("./src/main/resources/cr/bastriCris.xml"));
+
+			res += gson.toJson(centreRecherche);
+
+			// System.out.println("resultat ="+ res);
+			return res += "";
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "fail";
+
+	}
+
+	/**
+	 * retourne le centre de recheche qui correspond au siid
+	 * 
+	 * @param siid
+	 * @return
+	 * @throws JAXBException
+	 */
+	public Cr getCr(String siid) throws JAXBException {
+		JAXBContext jc;
+		jc = JAXBContext.newInstance("org.inria.fr.ns.cr");
+
+		Unmarshaller unmarshaller = jc.createUnmarshaller();
+
+		Crs centreRecherche = (Crs) unmarshaller.unmarshal(new File("./src/main/resources/cr/bastriCris.xml"));
+		for (Cr cr : centreRecherche.getCr()) {
+			if (cr.getSiid().compareToIgnoreCase(siid) == 0) {
+				return cr;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * retourne le centre de recheche qui correspond au siid
+	 * Service Rest
+	 * @param siid
+	 * @return
+	 */
+	@Path("getCr-{siidCr}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getCrRest(@PathParam("siidCr") String siid) {
+
+		final GsonBuilder builder = new GsonBuilder();
+		final Gson gson = builder.create();
+		Cr cr;
+		String res = "";
+		try {
+			cr = getCr(siid);
+			res += gson.toJson(cr);
+
+			// System.out.println("resultat ="+ res);
+			return res += "";
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "fail";
+
+	}
+}
---
+++ xmlRest/src/main/java/project/dataflow/ProjetManager.java
@@ -0,0 +1,107 @@
+package project.dataflow;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.plaf.basic.BasicScrollPaneUI.HSBChangeListener;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.inria.fr.ns.cr.Crs;
+import org.inria.fr.ns.cr.Crs.Cr;
+import org.inria.fr.ns.sr.Entite;
+import org.inria.fr.ns.sr.StructureInrias;
+import org.inria.fr.ns.sr.Structureinria;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Path("sr")
+public class ProjetManager {
+	@Path("listProject")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String listProject() {
+		JAXBContext jc;
+		final GsonBuilder builder = new GsonBuilder();
+	    final Gson gson = builder.create();
+		try {
+			jc = JAXBContext.newInstance("org.inria.fr.ns.sr");
+			String res = "";
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			
+			 StructureInrias sis = (StructureInrias) unmarshaller.unmarshal(new File("./src/main/resources/sr/bastri.xml"));
+			
+		
+				res += gson.toJson(sis);
+				
+			
+			
+			//System.out.println("resultat ="+ res);
+			return res;
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "fail";
+
+	}
+	
+	public HashMap<String, Integer> compteTeamParCentre() throws JAXBException{
+	    HashMap<String, Integer> hashCR = new HashMap<String,Integer>();
+		JAXBContext jc;
+	
+			jc = JAXBContext.newInstance("org.inria.fr.ns.sr");
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			
+			 StructureInrias sis = (StructureInrias) unmarshaller.unmarshal(new File("./src/main/resources/sr/bastri.xml"));
+			 List<Structureinria> si = sis.getStructureinria();
+		for (Structureinria structureinria : si) {
+			for(Entite entite :structureinria.getEntite()){
+				String siid = entite.getAdressegeographique().getCri().getSiid();
+				System.out.println(siid);
+				if(hashCR.containsKey(siid)){hashCR.put(siid, hashCR.get(siid)+1);}
+				else{
+					hashCR.put(siid, 1);
+
+				}
+			}
+		}
+		return hashCR;
+	}
+	
+
+	
+	@Path("teamParCentre")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String teamParCentre() {
+		final GsonBuilder builder = new GsonBuilder();
+	    final Gson gson = builder.create();
+		try {
+			HashMap<String, Integer> hashCR = compteTeamParCentre();
+			
+			String res = gson.toJson(hashCR);
+			//System.out.println("resultat ="+ res);
+			return res;
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "fail";
+	}
+	
+
+
+}
