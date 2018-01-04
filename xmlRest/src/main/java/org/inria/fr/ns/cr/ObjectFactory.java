4867fbd97129a3c2083c8fe750e3e27bf68a125c
DogiDogiDog <williamzabo@gmail.com>
2018-01-04 19:37:04 +0100

    Ajout Fop dans POM

--- xmlRest/src/main/java/org/inria/fr/ns/cr/ObjectFactory.java
+++ xmlRest/src/main/java/org/inria/fr/ns/cr/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.11 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2018.01.03 à 05:40:56 PM CET 
+// Généré le : 2018.01.04 à 07:35:36 PM CET 
 //
 
 
