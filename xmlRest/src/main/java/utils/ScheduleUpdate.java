7d2e7d56bf8ddb737efa040e2dce1ba75fdc666b
Balbis Robin <robin.balbis@etu.unice.fr>
2018-01-06 17:47:20 +0100

    ajout du rechargement programme des flux de données

--- xmlRest/src/main/java/utils/ScheduleUpdate.java
+++ xmlRest/src/main/java/utils/ScheduleUpdate.java
@@ -4,13 +4,29 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 import project.http.request.GetRequest;
 import project.http.request.HUCManager;
 
-public class ScheduleUpdate {
+public class ScheduleUpdate extends TimerTask{
 	
-	public static void run(){
+	public static void main(String[] args) {
+		
+		Calendar today = Calendar.getInstance();
+		today.set(Calendar.HOUR_OF_DAY, 2);
+		today.set(Calendar.MINUTE, 0);
+		today.set(Calendar.SECOND, 0);
+
+		// every night at 2am you run your task
+		Timer timer = new Timer();
+		timer.schedule(new ScheduleUpdate(), today.getTime(), TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
+	}
+	
+	public void run(){
 	try {
 		HttpURLConnection bastriCo = GetRequest.request("http://www-sop.inria.fr/members/Philippe.Poulard/projet/2017/bastri.xml");
 		HUCManager conn = new HUCManager(bastriCo);
